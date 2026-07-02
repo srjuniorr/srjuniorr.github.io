6bebacb5d71d82d7275b47b7db96da121f1d8587
srjuniorr <diasslrr@gmail.com>
2026-07-02 17:53:44 -0600

    remove old wdd231 copy

--- wdd231/scripts/date.js
+++
@@ -1,6 +0,0 @@
-// puts the current year and the last modified date in the footer
-
-const today = new Date();
-document.querySelector("#currentyear").textContent = today.getFullYear();
-
-document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
